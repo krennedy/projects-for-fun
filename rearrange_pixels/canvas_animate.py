b29c7de58b59e591f13595e4afb7976c14218e02
Rachel Kennedy <rkennedy292@gmail.com>
2016-03-27 15:21:02 -0700

    reduced dpi, reduced figs

--- rearrange_pixels/canvas_animate.py
+++ rearrange_pixels/canvas_animate.py
@@ -5,8 +5,8 @@ import matplotlib.animation as animation
 
 # GLOBALS
 FIG = plt.figure(figsize=(14,10)) # Define out fig
-SAVE_SNAPS = False
-NSTEPS = 50 # How many timesteps to use in animation
+SAVE_SNAPS = True
+NSTEPS = 25 # How many timesteps to use in animation
 
 class Animator():
 
@@ -92,7 +92,7 @@ class Animator():
             self.im2.set_array(self.ref2)
 
         if SAVE_SNAPS == True:
-            plt.savefig('saved_snaps/ex_%s.png'%j)
+            plt.savefig('saved_snaps/ex_%s.png'%j, dpi=40)
 
 
     def take_out(self, img, pix, j):
