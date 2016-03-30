d1ce49e9e49ffa949a48eb2f0a3dfccc4a9b61dc
Rachel Kennedy <rkennedy292@gmail.com>
2016-03-30 16:10:04 -0700

    documentation

--- rearrange_pixels/utils/pixel_tracker.py
+++ rearrange_pixels/utils/pixel_tracker.py
@@ -3,15 +3,40 @@ import numpy as np
 
 
 class PixelTracker():
-    """
-    
+    """ This class parses the information about an input image,
+    including the spatial information about each RGB pixel. #FIXME: elaborate better!
+
+    Inputs
+    ---------
+    path_to_image: The path to an input image, of type .jpg, .png, etc.
+
+    Attributes
+    ---------
+    shape: tuple containing dimensions of the input image
+    xy: 2d-array describing the coordinates of each pixel in the
+        original image. The first and second columns contain the x and
+        y coordinates, respectively.
+    RGB: 2d-array describing the color of each pixel.
+        The three columns contain the R, G and B values ranging 0-255.
+    xy_new: 2d-array describing the coordinates of each pixel in the
+        *reconstructed* image, e.g. indicating where each RGB value
+        will move to in the new image display.
+
+    Methods
+    ---------
+    sort_by_fancybins(): reorder the pixels weighting both darkness and
+        color. The pixels are coarse-grained by darkness first, and
+        within this coarse-graining, ordered by 'colorwheel' color.
+    rearrange_pixels(): Takes input which is also an instance of a
+        PixelTracker object, and which has also had sort_by_fancybins()
+        applied. Maps new coordinates xy_new onto the original image.
     """
 
-    def __init__(self, path_to_jpg):
+    def __init__(self, path_to_image):
         
         """ Blah.
         """
-        img = Image.open(path_to_jpg)
+        img = Image.open(path_to_image)
         self.shape = img.size[::-1]  # reverse order
 
         img_pix = np.asarray(img)
