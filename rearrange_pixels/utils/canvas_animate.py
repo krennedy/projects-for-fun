a6853f7f2aa7d0b79942d5cffca5f31045886a65
Rachel Kennedy <rkennedy292@gmail.com>
2016-03-30 18:01:32 -0700

    documentation

--- rearrange_pixels/utils/canvas_animate.py
+++ rearrange_pixels/utils/canvas_animate.py
@@ -1,47 +1,81 @@
+""" This contents of this module contain the instructions on how to
+display an animation of the pixel rearrangement.
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
 
 
-# GLOBALS
-FIG = plt.figure(figsize=(14,10)) # Define out fig
+# Global variables
+# -------------------------------------------------
+# Figure object
+FIG = plt.figure(figsize=(14,10))
+
+# FIXME: this should be kwarg, not hardcode
 SAVE_SNAPS = False
-NSTEPS = 25 # How many timesteps to use in animation
+
+# The number of time-steps to use in the animation
+NSTEPS = 25
+# -------------------------------------------------
+
 
 class Animator():
+    """
+    Attributes
+    ---------
+    pixA: PixelTracker object containing data about one image
+    pixB: PixelTracker object containing data about another image
 
+    map1: original image from pixA (gets depleted in animation)
+    map2: original image from pixB (gets depleted in animation)
+    map3: rearranged image of pixels from pixA (starts blank)
+    map4: rearranged image of pixels from pixB (starts blank)
+    ref1: copy of map1 (b/c map1 will get modified in animation)
+    ref2: copy of map2 (b/c map1 will get modified in animation)
 
-    def __init__(self):
-        # im1 : the imshow at ax1
-        # map1: the rgb pixel map displayed in im1
-        # map3: same pixels as map1, but rearranged to look like pix2
-        pass
+    im[1-4]: The imshow() objects for each subplot (get modified)
+
+    npix: Number of pixels in the images
+    nstep: How many pixels will be 'rearranged' in each step of the animation
+    """
 
     def load(self, pixA, pixB):
-        """ Wow, we need a docstring!
         """
-        self.pixA = pixA  # Blaaaaahhhhh
-        self.pixB = pixB  # Blaaahhhhhhh
+        Load in pixA and pixB (PixelTracker objects with information
+        about the pixels of two images).
 
-        # Initial displays (top row), just the original images
+        This method uses this information to create 4 'maps' which are
+        in the correct format to be displayed by imshow().
+
+        Also calculate the total number of pixels, and number to be
+        updated per time-step.
+        """
+        self.pixA = pixA
+        self.pixB = pixB
+
+        # Initial displays (top row), just the original images.
         rgb1 = sort_rgb_into_image_order(pixA.RGB, pixA.xy)
         self.map1 = convert_to_imshow_format(rgb1, pixA.shape)
 
         rgb2 = sort_rgb_into_image_order(pixB.RGB, pixB.xy)
         self.map2 = convert_to_imshow_format(rgb2, pixB.shape)
 
-        # New displays (bottom row) START WHITE
+        # New displays (bottom row) - these start white and get filled in.
         self.map3 = make_white_image(pixB.shape)
         self.map4 = make_white_image(pixA.shape)
 
-        self.npix = self.map1.shape[0] * self.map1.shape[1]
-        self.nstep = int(self.npix / float(NSTEPS))  # number PER step
-
-        # The original maps will get modified as you go, so keep a reference
+        # The original maps will get depleted as you go, so keep a reference.
         self.ref1 = self.map1.copy()
         self.ref2 = self.map2.copy()
 
+        # The total number of pixels, and number to change each time-step.
+        self.npix = self.map1.shape[0] * self.map1.shape[1]
+        self.nstep = int(self.npix / float(NSTEPS))  # number PER step
+
     def initialize_canvas(self):
+        """ Create 4 subplots and display map[1-4] in them.
+        """
         kwargs = dict(animated=True, interpolation='none')
 
         ax1 = FIG.add_subplot(221)
@@ -64,19 +98,27 @@ class Animator():
         plt.title('Image 2 Rearranged')
         plt.axis('off')
 
-    def draw(self,):
+    def draw(self, skip_animation=False):
+        """ Do the animation (alternatively can just skip to the end
+        result of rearrangement, if skip_animation is set to True.)
+        """
         self.initialize_canvas()
-        draw_me = animation.FuncAnimation(
-            FIG, self.updatefig,
-            np.arange(0, NSTEPS),
-            interval=1, blit=False, repeat=False)
+
+        if not skip_animation: # FIXME: handle else True
+            draw_me = animation.FuncAnimation(
+                FIG, self.updatefig,
+                np.arange(0, NSTEPS),
+                interval=1, blit=False, repeat=False)
         plt.show()
 
     def updatefig(self, j):
-        """ Update all 4. Think rgb_A is now called map1.
+        """ Each timestep, we will be removing a subset of pixels
+        from the top row images, and inserting them into the bottom
+        row images.
+        Save the outputs if desired. I use to make .gif summary.
         """
-        self.take_out(self.map1, self.pixA, j)
-        self.take_out(self.map2, self.pixB, j)
+        self.take_out(self.map1, j)
+        self.take_out(self.map2, j)
         self.put_in(self.ref1, self.map3, self.pixA, j)
         self.put_in(self.ref2, self.map4, self.pixB, j)
 
@@ -85,7 +127,7 @@ class Animator():
         self.im3.set_array(self.map3)
         self.im4.set_array(self.map4)
 
-        # return top row to original state if at end of animation
+        # When we reach end of animation, revert top row to original images.
         if j >= NSTEPS - 1:
             self.im1.set_array(self.ref1)
             self.im2.set_array(self.ref2)
@@ -94,7 +136,10 @@ class Animator():
             plt.savefig('saved_snaps/ex_%s.png'%j, dpi=20)
 
 
-    def take_out(self, img, pix, j):
+    def take_out(self, img, j):
+        """ Take some (j*nstep) pixels out of img, and replace them
+        with grey (R = G = B = 155).
+        """
         initial_shape = img.shape
         img_flat = img.reshape(self.npix, 3)
         start_idx = j * self.nstep
@@ -105,8 +150,9 @@ class Animator():
 
 
     def put_in(self, img_pull, img_put, pix, j):
-        """ Here, you want to put in a few pixels of new image
-        But using the values input from another image
+        """ Take some (j*nstep) pixels from the reference (img_pull),
+        and put them into the target (img_put), in the correct
+        positions for their specified rearrangement.
         """
         initial_shape = img_put.shape
 
